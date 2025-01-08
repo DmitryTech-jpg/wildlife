616c59303bf6066d57e01dd3355f26e453fe0018
Dmitry Sergeev <lbvfcthuttd1234@gmail.com>
2025-01-08 19:41:14 +0400

    start modal

--- src/script.js
+++ src/script.js
@@ -20,3 +20,20 @@
         menu.classList.remove('header__nav_active');
     });
 }());
+
+var modal__window = document.getElementById('modal');
+var btn = document.getElementById("btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal__window.style.display = "block";
+}
+span.onclick = function() {
+    modal__window.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal__window) {
+        modal__window.style.display = "none";
+    };
+}
